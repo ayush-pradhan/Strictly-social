359b974a4c20272d7ea6b0f1c33befed8b6c2544
Ayush Pradhan <ayupradhan1999@gmail.com>
2021-04-20 12:52:42 +0530

    Articles

--- src/public/app/socialCommon.js
+++ src/public/app/socialCommon.js
@@ -1,3 +1,27 @@
 $(() => {
-    $('#navbar').load('./components/navbar.html')
+    $('#navbar').load('./components/navbar.html', loginIfNeeded)
+    $('#footer').load('./components/footer.html')
+    $('#content').load('./components/all-articles.html')
 })
+
+function loginIfNeeded() {
+    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if (!currentUser) {
+        $.post('/api/users', {}, (user) => {
+            if (user) {
+                console.log('Registered user', user.username)
+                window.localStorage.user = JSON.stringify(user)
+                currentUser = user
+                $('#nav-username').text(user.username)
+            }
+        })
+    } else {
+        console.log("Resuming session as ", currentUser.username)
+        $('#nav-username').text(currentUser.username)
+
+    }
+}
+
+function loadArticles() {
+    $.get('/api/get')
+}
