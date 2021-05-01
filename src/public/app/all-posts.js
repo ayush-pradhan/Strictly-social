b9c040a48e885b1b436ee72e0724ab7244ed5a2c
Ayush Pradhan <ayupradhan1999@gmail.com>
2021-05-01 19:53:48 +0530

    Post articles

--- src/public/app/all-posts.js
+++ src/public/app/all-posts.js
@@ -1,21 +1,23 @@
 function loadArticles() {
     $.get('/api/posts', (posts) => {
         for( let p of posts){
-            console.log(p)
+            // console.log(p)
                 // $('.card-title').text(p.title)
                 // $('.card-text').text(p.body)
             $('#posts-container').append(
                 $(` 
-                    <div class = "col-4">
-                        <div class="card m-2" style="width: 18rem; z-index: -1;">
+                    <div class="col-4">
+                        <div class="card m-2" style=" z-index: -1;">
                             <div class="card-body">
                                 <h5 class="card-title">${p.title}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">
-                                    ${p.user.username}
-                                </h6>
-                                <p class="card-text">
-                                    ${p.body.substr(0, 200)}<a href="#">...read more</a>
-                                </p>
+                                <div class="card-info">
+                                    <h6 class="card-subtitle mb-2 text-muted">
+                                        ${p.user.username}
+                                    </h6>
+                                    <p class="card-text">
+                                        ${p.body.substr(0, 200)}<a href="#">...read more</a>
+                                    </p>
+                                </div>
                                 <a href="#" class="card-link">Comment</a>
                                 <a href="#" class="card-link">Like</a>
                             </div>
---
+++ src/public/app/navbar.js
@@ -0,0 +1,5 @@
+let navLinks = $('.navbar-nav .nav-link')
+navLinks.click((ev) => {
+    let componentUrl = `/components/${$(ev.target).attr('data-component')}.html`
+    $('#content').load(componentUrl)
+})
